ce98b1ffd07977df2e750c0e329f9047db0e0485
YannickOclock <yannick.kuhn@oclock.io>
2024-04-22 23:50:01 +0200

    Mise en place du badge user et navigation conditionnelle

--- start/routes.ts
+++ start/routes.ts
@@ -9,13 +9,13 @@
 
 import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
 router.on('/').render('pages/home').as('home')
 
-router.get('/register', [AuthController, 'register']).as('auth.register')
-router.post('/register', [AuthController, 'handleRegister'])
+router.get('/register', [AuthController, 'register']).as('auth.register').use(middleware.guest())
+router.post('/register', [AuthController, 'handleRegister']).use(middleware.guest())
+router.get('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest())
+router.post('/login', [AuthController, 'handleLogin']).use(middleware.guest())
 
-router.get('/login', [AuthController, 'login']).as('auth.login')
-router.post('/login', [AuthController, 'handleLogin'])
-
-router.delete('/logout', [AuthController, 'logout']).as('auth.logout')
+router.delete('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
